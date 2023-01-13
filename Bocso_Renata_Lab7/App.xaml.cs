c60dfbb05b0805b857b0cf42869e03163aaa3060
renata.bocso <renatabocso@gmail.com>
2023-01-13 19:10:43 +0200

    Lab 7

---
+++ Bocso_Renata_Lab7/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Bocso_Renata_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Bocso_Renata_Lab7/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Bocso_Renata_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
