895be397b0d7075f960ca5b0e233d9cacc230728
renata.bocso <renatabocso@gmail.com>
2023-01-13 19:26:29 +0200

    Lab 8

--- Bocso_Renata_Lab7/App.xaml.cs
+++ Bocso_Renata_Lab7/App.xaml.cs
@@ -1,8 +1,25 @@
-﻿namespace Bocso_Renata_Lab7;
+﻿using Bocso_Renata_Lab7.Data;
+
+namespace Bocso_Renata_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
