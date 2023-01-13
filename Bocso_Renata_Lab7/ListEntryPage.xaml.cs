a8982f0a1c00bb85cc0fc69adf3653ac92d11d28
renata.bocso <renatabocso@gmail.com>
2023-01-13 20:10:52 +0200

    Lab9

--- Bocso_Renata_Lab7/ListEntryPage.xaml.cs
+++ Bocso_Renata_Lab7/ListEntryPage.xaml.cs
@@ -1,9 +1,11 @@
 using Bocso_Renata_Lab7.Models;
+using SQLitePCL;
 
 namespace Bocso_Renata_Lab7;
 
 public partial class ListEntryPage : ContentPage
 {
+    private object _selectedItem;
 	public ListEntryPage()
 	{
 		InitializeComponent();
@@ -14,6 +16,12 @@ public partial class ListEntryPage : ContentPage
         base.OnAppearing();
         listView.ItemsSource = await App.Database.GetShopListsAsync();
     }
+
+    async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        var selectedItem = _selectedItem;
+    }
+
     async void OnShopListAddedClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new ListPage
@@ -25,6 +33,7 @@ public partial class ListEntryPage : ContentPage
     {
         if (e.SelectedItem != null)
         {
+            _selectedItem = e.SelectedItem;
             await Navigation.PushAsync(new ListPage
             {
                 BindingContext = e.SelectedItem as ShopList
--- Bocso_Renata_Lab7/ListPage.xaml.cs
+++ Bocso_Renata_Lab7/ListPage.xaml.cs
@@ -22,4 +22,22 @@ public partial class ListPage : ContentPage
         await Navigation.PopAsync();
     }
 
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
 }
